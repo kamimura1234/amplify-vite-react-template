d0ba60cbb76da8d33dd830603018cf04b04c6c11
sso-kamimura <kamimura_yukinari@koushi-intec.co.jp>
2025-03-13 13:56:29 +0900

    added per-user data isolation2

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,6 @@ function App() {
   return (
     <main>
       <button onClick={signOut}>Sign out</button>
-      <h1>My todos</h1>
       <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
